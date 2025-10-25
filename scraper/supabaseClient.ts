27f6a34cabd103a8ab0ea8f98bfe9e263c734459
Abby Lossa <abby.lossa@gmail.com>
2025-10-25 15:14:01 -0500

    supabase client

--- scraper/supabaseClient.ts
+++ scraper/supabaseClient.ts
@@ -4,6 +4,10 @@ import * as dotenv from "dotenv";
 // Explicitly load the .env file in this folder
 dotenv.config({ path: ".env" });
 
+// DEBUG LOGS - TEMPORARY
+console.log("[DEBUG] SUPABASE_URL:", process.env.SUPABASE_URL);
+console.log("[DEBUG] SUPABASE_SERVICE_KEY defined?:", !!process.env.SUPABASE_SERVICE_KEY);
+
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY;
 

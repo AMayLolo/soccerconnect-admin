84f8a5fe958b5ecd55bbe955818798e6f6e04534
Abby Lossa <abby.lossa@gmail.com>
2025-10-26 09:58:49 -0500

    chore: stop tracking .next so Vercel sees correct Next.js package.json

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4jJSudyzFt1/RnigtiOXBOmkfEX08qA6jZC+8EHi5oI=\"\n}"
--- .next/dev/static/chunks/src_app_login_page_tsx_3a71bcca._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_@supabase_node-fetch_browser_ebbd8575.js",
-  "static/chunks/node_modules_330cf17f._.js",
-  "static/chunks/src_app_login_LoginClient_tsx_c032f046._.js"
-],
-    source: "dynamic"
-});
